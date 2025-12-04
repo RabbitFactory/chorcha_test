2f00bd66d42fbb88c4f554c6edd0ff4c7761c7a8
Shahi <jv16th.raj@gmail.com>
2025-12-04 13:40:47 +0600

    nav and hero done

--- app/product/components/Hero.jsx
+++ app/product/components/Hero.jsx
@@ -2,7 +2,20 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className='flex gap-40 justify-center items-center mt-32'>
+        <div className='max-w-[500px] space-y-4'>
+            <h1 className='text-5xl'>চর্চা করো নিজের গতিতে</h1>
+            <p>সিলেবাসের যেকোন টপিক থেকে প্রশ্ন খুঁজে প্র্যাকটিস করো, AI দিয়ে ব্যাখ্যা বুঝে নাও, আর মক টেস্ট দিয়ে নিজের প্রস্তুতি মাপো।</p>
+            <div className='flex gap-5'>
+                <img src="/store.png" alt="" />
+            <img src="/apple.png" alt="" />
+            </div>
+            <p>100k+ downloads |       4.6 Rating with over 8k Reviews</p>
+        </div>
+        <div>
+            <img className='w-64' src="/Mockup 2.png" alt="" />
+        </div>
+    </div>
   )
 }
 
